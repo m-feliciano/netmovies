3cae3e83a37e6d4b4e13801aaca4e551470bbaf1
Marcelo Feliciano <60995044+m-feliciano@users.noreply.github.com>
2022-01-10 15:50:21 -0300

    Form Layout

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,6 +1,43 @@
+import "./styles.css"
+
 function Listing() {
+
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
+
+
     return (
-        <h1>Form Page</h1>
+        <div className="netmovies-form-container">
+            <img className="netmovies-movie-card-image" src={movie.image} alt={movie.title}/>
+            <div className="netmovies-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <form className="netmovies-form">
+                    <div className="form-group netmovies-form-group">
+                        <label htmlFor="email">Enter email</label>
+                        <input type="email" className="form-control" id="email"/>
+                    </div>
+                    <div className="form-group netmovies-form-group">
+                        <label htmlFor="score">Report your review</label>
+                        <select className="form-control" id="score">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="netmovies-form-btn-container">
+                        <button type="submit" className="btn btn-primary netmovies-btn">Save</button>
+                    </div>
+                </form>
+                <button className="btn btn-primary netmovies-btn mt-3">Cancel</button>
+            </div>
+        </div>
     )
 }
 
