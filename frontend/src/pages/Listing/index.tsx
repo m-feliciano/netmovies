7188ad88834325bb33545690b8609b9fcfead751
Marcelo Feliciano <60995044+m-feliciano@users.noreply.github.com>
2022-01-16 19:16:37 -0300

    useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Pagination from "../Pagination";
 import MovieCard from "../../components/MovieCard";
+import {BASE_URL} from "../../utils/requests";
+import axios from "axios";
+import {MoviePage} from "../../types/movie";
 
 function Form() {
+
+    const [PageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/api/movies`)
+            .then(res => {
+                const data = res.data as MoviePage;
+                console.log(data);
+                setPageNumber(data.number);
+            });
+    }, []);
+
+
     return (
         <>
+            <p>{PageNumber}</p>
             <Pagination/>
-            <div className="Container">
+            <div className="container">
                 <div className="row">
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard/>
