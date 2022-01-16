9362fa8fbbe342637f86ca2468455d4fce764c36
Marcelo Feliciano <60995044+m-feliciano@users.noreply.github.com>
2022-01-16 19:36:30 -0300

    props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -9,38 +9,38 @@ function Form() {
 
     const [PageNumber, setPageNumber] = useState(0);
 
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
+
     useEffect(() => {
-        axios.get(`${BASE_URL}/api/movies`)
+        axios.get(`${BASE_URL}/api/movies?size=12&page=${PageNumber}&sort=id`)
             .then(res => {
                 const data = res.data as MoviePage;
-                console.log(data);
-                setPageNumber(data.number);
+                setPage(data);
             });
-    }, []);
+    }, [PageNumber]);
 
-
-    return (
-        <>
-            <p>{PageNumber}</p>
-            <Pagination/>
-            <div className="container">
-                <div className="row">
+    return <>
+        <Pagination/>
+        <div className="container">
+            <div className="row">
+                {page.content.map((movie) => (
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard/>
+                        <MovieCard movie={movie}/>
                     </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard/>
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard/>
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard/>
-                    </div>
-                </div>
+                ))};
             </div>
-        </>
-    )
+        </div>
+    </>
 }
 
 export default Form
